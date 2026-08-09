c751fed6c2d0715679d20015bef4b5dd7e970f3f
huyhduve <tohuynhducno2@gmail.com>
2026-08-09 22:06:20 +0700

    feat(schemas): update schemas for featuring ASR and OCR search

--- app/schemas/request.py
+++ app/schemas/request.py
@@ -1,7 +1,12 @@
 from pydantic import BaseModel, Field
 
 class TextQueryRequest(BaseModel): 
-    query : str = Field(..., min_length=1, max_length=500, examples=["Donald Trump"])
+    text : str = Field(..., max_length=500, examples=["Donald Trump"])
+    text_score: float = Field(default=0, ge=0, le=1)
+    asr : str = Field(..., max_length=200)
+    asr_score: float = Field(default=0, ge=0, le=1)
+    ocr : str = Field(..., max_length=200)
+    ocr_score : float = Field(default=0, ge=0, le=1)
     top_k : int = Field(default=100, ge=1, le=200) 
 
 
--- app/schemas/response.py
+++ app/schemas/response.py
@@ -2,9 +2,15 @@ from pydantic import BaseModel
 
 class ImageResult(BaseModel): 
     image_id: str
+    ocr_text: str  
+    asr_text: str 
+
+
+class GroupImageResult(BaseModel): 
+    groups: list[ImageResult]
 
 class ImageSearchResponse(BaseModel):
-    results: list[ImageResult]
+    results: list[GroupImageResult]
 
 class ErrorResponse(BaseModel): 
     description: str 
