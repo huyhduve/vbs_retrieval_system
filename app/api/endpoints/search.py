9a313c3b0c79fbbfa0b2d8ecb1af559add5d6a1d
huyhduve <tohuynhducno2@gmail.com>
2026-08-21 11:34:03 +0700

    feat(search): add image based search module & endpoint

--- app/api/endpoints/search.py
+++ app/api/endpoints/search.py
@@ -1,16 +1,43 @@
 import numpy as np 
 import os 
+from PIL import Image
+import io 
+from typing import List
 
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, HTTPException, status, UploadFile, File
 from schemas.response import ImageResult, ImageSearchResponse, GroupImageResult
 from schemas.request import QueryRequest, SimSearchRequest
 from config import settings
 router = APIRouter()
 
+def _format_milvus_res(hits : List[dict]): 
+    groups_map = {}
+
+    for hit in hits:
+        raw_id = str(hit["id"]) 
+        
+        video_id = raw_id.split("/")[0] if "/" in raw_id else raw_id
+        
+        image_item = ImageResult(
+            image_id=f"{raw_id}.webp",
+            ocr_text="",
+            asr_text=""
+        )
+        
+        if video_id not in groups_map:
+            groups_map[video_id] = []
+        groups_map[video_id].append(image_item)
+
+    grouped_results = [
+        GroupImageResult(video_id=v_id, group=images)
+        for v_id, images in groups_map.items()
+    ]
+
+    return ImageSearchResponse(results=grouped_results)
+
 
 @router.post("/search", response_model=ImageSearchResponse)
-def search(query: QueryRequest | SimSearchRequest, requests: Request):
-
+def text_base_search(query: QueryRequest | SimSearchRequest, requests: Request):
     encoder = requests.app.state.encoder
     milvus_client = requests.app.state.client
 
@@ -33,28 +60,48 @@ def search(query: QueryRequest | SimSearchRequest, requests: Request):
         limit=TOP_K
     )
 
-    hits = results[0]
+    return _format_milvus_res(results[0])
+    
 
-    groups_map = {}
+ALLOW_EXTENSIONS = {"image/jpeg", "image/png", "image/webp"}
+MAX_FILE_SIZE = 1.5 * 1024 * 1024 # MAX 1.5MB 
 
-    for hit in hits:
-        raw_id = str(hit["id"])  # VD: "L21_V023/234"
-        
-        video_id = raw_id.split("/")[0] if "/" in raw_id else raw_id
-        
-        image_item = ImageResult(
-            image_id=f"{raw_id}.webp",
-            ocr_text="",
-            asr_text=""
+@router.post("/search/image", response_model=ImageSearchResponse)
+def image_base_search(requests : Request, file : UploadFile = File(...)):
+    if file.content_type not in ALLOW_EXTENSIONS: 
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, 
+            detail="Sai định dạng [.jpeg, .png, .webp]"
         )
-        
-        if video_id not in groups_map:
-            groups_map[video_id] = []
-        groups_map[video_id].append(image_item)
 
-    grouped_results = [
-        GroupImageResult(video_id=v_id, group=images)
-        for v_id, images in groups_map.items()
-    ]
+    contents = file.file.read()
 
-    return ImageSearchResponse(results=grouped_results)
+    if len(contents) > MAX_FILE_SIZE:
+        raise HTTPException(
+            status_code=status.HTTP_413_REQUEST_ENTITY_TOO_LARGE,
+            detail="Kích thước file vượt quá giới hạn 1.5MB."
+        )
+
+    try: 
+        image = Image.open(io.BytesIO(contents)).convert("RGB")
+
+    except Exception: 
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, 
+            detail="File image bị lỗi"
+        )
+
+    encoder = requests.app.state.encoder 
+    milvus_client = requests.app.state.client
+
+    TOP_K = 100
+    embedding = encoder.encode_image(image)
+
+    results = milvus_client.search(
+            collection_name=settings.MILVUS_COLLECTION,
+            data=embedding.tolist(),
+            output_fields=["id"], 
+            limit=TOP_K
+        )
+    
+    return _format_milvus_res(results[0])
--- app/api/testing_endpoints/search.py
+++ app/api/testing_endpoints/search.py
@@ -1,148 +1,118 @@
-from fastapi import APIRouter, HTTPException, status, Request
+import numpy as np 
+import os 
+from PIL import Image
+import io 
+
+from fastapi import APIRouter, Request, HTTPException, status, UploadFile, File
+from pydantic import BaseModel
 from schemas.response import ImageResult, ImageSearchResponse, GroupImageResult
 from schemas.request import QueryRequest, SimSearchRequest
-from core.siglip_engine import SigLIP2Encoder
-from milvus.connection import get_client
-from config import settings
 
 router = APIRouter()
 
-@router.post("/search", response_model=ImageSearchResponse)
-async def search(query : QueryRequest | SimSearchRequest, requests : Request):
 
-    if (isinstance(query, QueryRequest)): 
-        if query.text == "r": 
-            response = ImageSearchResponse(results=[
-                GroupImageResult(
-                    video_id="L21_V001", 
-                    group=[
-                        ImageResult(
-                            image_id="L21_V001/0.webp",
-                            ocr_text="day la vi du ocr", 
-                            asr_text="day la vi du asr" 
-                        ), 
-                        ImageResult(
-                            image_id="L21_V001/19.webp",
-                            ocr_text="day la vi du ocr", 
-                            asr_text="day la vi du asr" 
-                        ), 
-                        ImageResult(
-                            image_id="L21_V001/324.webp",
-                            ocr_text="day la vi du ocr", 
-                            asr_text="day la vi du asr" 
-                        ), 
-                        ImageResult(
-                            image_id="L21_V001/403.webp",
-                            ocr_text="day la vi du ocr", 
-                            asr_text="day la vi du asr" 
-                        ), 
-                        
-                    ]
-                ), 
+class ImageMetadataResponse(BaseModel):
+    file_size: int
+    width: int
+    height: int
 
-                GroupImageResult(
-                            video_id="L21_V002", 
-                            group=[
-                                ImageResult(
-                                    image_id="L21_V002/0.webp",
-                                    ocr_text="day la vi du ocr", 
-                                    asr_text="day la vi du asr" 
-                                ), 
-                                ImageResult(
-                                    image_id="L21_V002/1605.webp",
-                                    ocr_text="day la vi du ocr", 
-                                    asr_text="day la vi du asr" 
-                                ), 
-                                ImageResult(
-                                    image_id="L21_V002/2541.webp",
-                                    ocr_text="day la vi du ocr", 
-                                    asr_text="day la vi du asr" 
-                                ), 
-                            ]
-                        ), 
-
-            ])
-        else: 
-            response = ImageSearchResponse(results=[
-                        GroupImageResult(
-                            video_id="L21_V002/2541.webp", 
-                            group=[
-                                ImageResult(
-                                    image_id="L21_V002/2541.webp",
-                                    ocr_text="day la vi du ocr", 
-                                    asr_text="day la vi du asr" 
-                                ), 
-                                ImageResult(
-                                    image_id="L21_V002/2859.webp",
-                                    ocr_text="day la vi du ocr", 
-                                    asr_text="day la vi du asr" 
-                                ), 
-                                ImageResult(
-                                    image_id="L21_V002/3160.webp",
-                                    ocr_text="day la vi du ocr", 
-                                    asr_text="day la vi du asr" 
-                                ), 
-                                ImageResult(
-                                    image_id="L21_V002/3235.webp",
-                                    ocr_text="day la vi du ocr", 
-                                    asr_text="day la vi du asr" 
-                                ), 
-                                
-                            ]
-                        ), 
-            
-                        GroupImageResult(
-                                    video_id="L21_V005", 
-                                    group=[
-                                        ImageResult(
-                                            image_id="L21_V005/0.webp",
-                                            ocr_text="day la vi du ocr", 
-                                            asr_text="day la vi du asr" 
-                                        ), 
-                                        ImageResult(
-                                            image_id="L21_V005/736.webp",
-                                            ocr_text="day la vi du ocr", 
-                                            asr_text="day la vi du asr" 
-                                        ), 
-                                        ImageResult(
-                                            image_id="L21_V005/868.webp",
-                                            ocr_text="day la vi du ocr", 
-                                            asr_text="day la vi du asr" 
-                                        ), 
-                                    ]
-                                ), 
-            
-                    ])
-    else : 
-        response = ImageSearchResponse(results=[
-                        GroupImageResult(
-                            video_id="L21_V001", 
-                            group=[
-                                ImageResult(
-                                    image_id="L21_V001/0.webp",
-                                    ocr_text="day la vi du ocr", 
-                                    asr_text="day la vi du asr" 
-                                ), 
-                                ImageResult(
-                                    image_id="L21_V001/19.webp",
-                                    ocr_text="day la vi du ocr", 
-                                    asr_text="day la vi du asr" 
-                                ), 
-                                ImageResult(
-                                    image_id="L21_V001/324.webp",
-                                    ocr_text="day la vi du ocr", 
-                                    asr_text="day la vi du asr" 
-                                ), 
-                                ImageResult(
-                                    image_id="L21_V001/403.webp",
-                                    ocr_text="day la vi du ocr", 
-                                    asr_text="day la vi du asr" 
-                                ), 
-                                
-                            ]
-                        ), 
-        ])
-
+def _format_milvus_res(): 
+    response = ImageSearchResponse(results=[
+                    GroupImageResult(
+                        video_id="L21_V001", 
+                        group=[
+                            ImageResult(
+                                image_id="L21_V001/0.webp",
+                                ocr_text="day la vi du ocr", 
+                                asr_text="day la vi du asr" 
+                            ), 
+                            ImageResult(
+                                image_id="L21_V001/19.webp",
+                                ocr_text="day la vi du ocr", 
+                                asr_text="day la vi du asr" 
+                            ), 
+                            ImageResult(
+                                image_id="L21_V001/324.webp",
+                                ocr_text="day la vi du ocr", 
+                                asr_text="day la vi du asr" 
+                            ), 
+                            ImageResult(
+                                image_id="L21_V001/403.webp",
+                                ocr_text="day la vi du ocr", 
+                                asr_text="day la vi du asr" 
+                            ), 
+                            
+                        ]
+                    ), 
     
+                    GroupImageResult(
+                                video_id="L21_V002", 
+                                group=[
+                                    ImageResult(
+                                        image_id="L21_V002/0.webp",
+                                        ocr_text="day la vi du ocr", 
+                                        asr_text="day la vi du asr" 
+                                    ), 
+                                    ImageResult(
+                                        image_id="L21_V002/1605.webp",
+                                        ocr_text="day la vi du ocr", 
+                                        asr_text="day la vi du asr" 
+                                    ), 
+                                    ImageResult(
+                                        image_id="L21_V002/2541.webp",
+                                        ocr_text="day la vi du ocr", 
+                                        asr_text="day la vi du asr" 
+                                    ), 
+                                ]
+                            ), 
+    
+                ])
+
     return response
+
+
+@router.post("/search", response_model=ImageSearchResponse)
+def text_base_search(query: QueryRequest | SimSearchRequest, requests: Request):
+    return _format_milvus_res()
+    
+
+ALLOW_EXTENSIONS = {"image/jpeg", "image/png", "image/webp"}
+MAX_FILE_SIZE = 1.5 * 1024 * 1024 # MAX 1.5MB 
+
+@router.post("/search/image", response_model=ImageMetadataResponse)
+def image_base_search(requests : Request, file : UploadFile = File(...)):
+    if file.content_type not in ALLOW_EXTENSIONS: 
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, 
+            detail="Sai định dạng [.jpeg, .png, .webp]"
+        )
+
+    contents = file.file.read()
+
+    if len(contents) > MAX_FILE_SIZE:
+        raise HTTPException(
+            status_code=status.HTTP_413_REQUEST_ENTITY_TOO_LARGE,
+            detail="Kích thước file vượt quá giới hạn 1.5MB."
+        )
+
+    try: 
+        image = Image.open(io.BytesIO(contents)).convert("RGB")
+
+    except Exception: 
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, 
+            detail="File image bị lỗi"
+        )
+
+    file_size = len(contents)
+    width, height = image.size
+
+    return ImageMetadataResponse(
+        file_size=file_size,
+        width=width,
+        height=height,
+    )
+
+
+
 
